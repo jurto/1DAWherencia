18afd66beb9b52eab0538fea8a0b1f22b5c4dd9c
PixelZer0 <pixelzero@gmail.com>
2017-02-01 12:28:59 +0100

    Clase "Empleado" completada

--- 1DAWherencia/src/modelo/Empleado.java
+++ 1DAWherencia/src/modelo/Empleado.java
@@ -6,4 +6,35 @@ package modelo;
  */
 public class Empleado {
     
+    // Variables
+    
+    protected String nombre;
+    
+    
+    // Métodos constructores
+    
+    public Empleado(String n) {
+        this.nombre = n;
+    }
+    
+    public Empleado() {
+    }
+    
+    
+    // Métodos funcionales
+
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "nombre=" + nombre + '}';
+    }
+    
 }
