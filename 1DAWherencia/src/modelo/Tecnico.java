bc2929582c780afd394fa96ea1ed76160e1b3316
a022593391p <a022593391p@s09-pc03>
2017-02-01 12:41:31 +0100

    Se ha añadido Tecnico que desciende de Operario

---
+++ 1DAWherencia/src/modelo/Tecnico.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author a022593391p
+ */
+public class Tecnico extends Operario{
+   
+public Tecnico(){}
+
+public Tecnico(String nombre){
+    super(nombre);
+}
+
+    @Override
+    public String toString() {
+        return "Tecnico{" + '}';
+    }
+
+
+}
